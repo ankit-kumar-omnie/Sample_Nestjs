e934afd1368ae93741eb4ae25606864465010bd9
Anant <trainee02@omniesolutions.com>
2023-09-18 13:48:43 +0530

    only admin-authorization

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,21 +8,6 @@ export class UserService {
         username: "admin",
         password: "admin02",
         email: "admin@gmail.com",
-    },
-    {
-        username: "user 1",
-        password: "user@01",
-        email: "user1@gmail.com",
-    },
-    {
-        username: "user 2",
-        password: "user@02",
-        email: "user2@gmail.com",
-    },
-    {
-        username: "user 3",
-        password: "user@03",
-        email: "user3@gmail.com",
     }]
 
 
